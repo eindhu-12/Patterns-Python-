22bb729ba058be0f1903297b1848e1d1f9fd3e69
unknown <rapoluindhu2929@gmail.com>
2025-02-03 22:48:37 +0530

    codes with outputs

--- numPatternT.py
+++ numPatternT.py
@@ -4,6 +4,13 @@ for i in range(1,n+1):
         print(j,end=" ") 
     print()
  
+#  Enter a number:-5
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
 m=int(input("Enter a number:-"))   
 for i in range(m):
     for j in range(m+i):
@@ -12,6 +19,13 @@ for i in range(m):
         else:
             print("*",end=" ")
     print()
+    
+# Enter a number:-5
+# * * * * *
+#   * * * * *
+#     * * * * *
+#       * * * * *
+#         * * * * *
 
 x=int(input("Enter a number:-"))
 for i in range(1,x+1):
@@ -28,4 +42,10 @@ for i in range(1,x+1):
                 print("0",end=" ")
     print()
             
+# Enter a number:-5
+# 1
+# 0 1
+# 1 0 1
+# 0 1 0 1
+# 1 0 1 0 1
     
