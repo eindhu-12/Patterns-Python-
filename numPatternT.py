b4bce9ba7875c05e1b0d11e3978d9cd94c0038c4
unknown <rapoluindhu2929@gmail.com>
2025-02-06 22:31:06 +0530

    Number and Star patterns

--- numPatternT.py
+++ numPatternT.py
@@ -1,51 +1,133 @@
-n=int(input("Enter a number:-"))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end=" ") 
-    print()
+# n=int(input("Enter a number:-"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ") 
+#     print()
  
-#  Enter a number:-5
-# 1
-# 1 2
-# 1 2 3
-# 1 2 3 4
-# 1 2 3 4 5
+# #  Enter a number:-5
+# # 1
+# # 1 2
+# # 1 2 3
+# # 1 2 3 4
+# # 1 2 3 4 5
 
-m=int(input("Enter a number:-"))   
-for i in range(m):
-    for j in range(m+i):
-        if j<i:
-            print(" ",end=" ")
-        else:
-            print("*",end=" ")
-    print()
+# m=int(input("Enter a number:-"))   
+# for i in range(m):
+#     for j in range(m+i):
+#         if j<i:
+#             print(" ",end=" ")
+#         else:
+#             print("*",end=" ")
+#     print()
     
-# Enter a number:-5
-# * * * * *
-#   * * * * *
-#     * * * * *
-#       * * * * *
-#         * * * * *
+# # Enter a number:-5
+# # * * * * *
+# #   * * * * *
+# #     * * * * *
+# #       * * * * *
+# #         * * * * *
 
-x=int(input("Enter a number:-"))
-for i in range(1,x+1):
-    for j in range(1,i+1):
-        if(i%2==0):
-            if j%2==0:
-                print("1",end=" ")
-            else:
-                print("0",end=" ")
-        else:
-            if j%2!=0:
-                print("1",end=" ")
-            else:
-                print("0",end=" ")
-    print()
+# # -----------Zero-One Triangle------------
+
+# x=int(input("Enter a number:-"))
+# for i in range(1,x+1):
+#     for j in range(1,i+1):
+#         if(i%2==0):
+#             if j%2==0:
+#                 print("1",end=" ")
+#             else:
+#                 print("0",end=" ")
+#         else:
+#             if j%2!=0:
+#                 print("1",end=" ")
+#             else:
+#                 print("0",end=" ")
+#     print()
             
-# Enter a number:-5
-# 1
-# 0 1
-# 1 0 1
-# 0 1 0 1
-# 1 0 1 0 1
+# # Enter a number:-5
+# # 1
+# # 0 1
+# # 1 0 1
+# # 0 1 0 1
+# # 1 0 1 0 1
+
+# # -----------Number Increasing Pyramid------------  
+
+# n=int(input("Enter a Number:-"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i<=j:
+#             print(j,end=" ")
+#     print()
+
+# # Enter a Number:-4
+# # 1 2 3 4
+# # 2 3 4
+# # 3 4
+# # 4
+
+# # -----------Number Triangular------------
+  
+# n=int(input("Enter a Number:-"))
+# for i in range(1,n+1):
+#     for j in range(n,0,-1):
+#         if (j<=i):
+#             print(i,end=" ")
+#         else:
+#             print(" ",end="")
+#     print()
     
+# # Enter a Number:-5
+# #     1
+# #    2 2
+# #   3 3 3
+# #  4 4 4 4
+# # 5 5 5 5 5
+
+# # -----------Number Changing Pyramid------------
+
+# n=int(input("Enter a Number of Rows:-"))
+# c=0
+# for i in range(1,n+1):
+#     for j in range(i):
+#         c+=1
+#         print(c,end=" ")
+#     print()
+
+# # Enter a Number of Rows:-5
+# # 1 
+# # 2 3
+# # 4 5 6
+# # 7 8 9 10
+# # 11 12 13 14 15
+
+n=int(input("Enter a Number of Rows:-"))
+col=(n*2)-1
+col_itr=0
+
+for i in range(n-1,-1,-1):
+    # print(i)
+    c=(n-i)
+    for j in range(col):
+        
+        if i>j :
+            print(" ",end=" ")
+        elif(j>n+col_itr):
+            print(" ",end=" ")
+            col_itr+=1
+        elif j<=n-1:
+            print(c,end=" ")
+            c-=1
+        else:
+            if c==0:
+                c+=2
+                print(c,end=" ")
+            else:
+                c+=1
+                print(c,end=" ")   
+    print()
+    
+        
+        
+
+
--- patterns.py
+++ patterns.py
@@ -1,84 +1,170 @@
-# n=int(input("Enter a Number:-"))
-# for i in range(n):
-#     for j in range(n):
-#         if(i==0 or i==n-1):
-#             print("*",end=" ")
-#         else:
-#             if j==0 or j==n-1:
-#                 print("*",end=" ")
-#             else:
-#                 print(" ",end=" ")
-#     print()
+# # ----------------Square Filled Pattern----------------
 
-# # Enter a Number:-5
-# # * * * * *
-# # *       *
-# # *       *
-# # *       *
-# # * * * * * 
-            
-# m=int(input("Enter a number:-"))
-# for i in range(1,m+1):
-#     for j in range(i):
-#         print("*",end=" ")
-#     print()
+a=int(input("Enter a Number:-"))
+for i in range(a):
+    for j in range(a):
+        print("*",end=" ")
+    print()
     
-# # Enter a number:-5
-# # *
-# # * *
-# # * * *
-# # * * * *
-# # * * * * *
+# Enter a Number:-5
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
 
-# k=int(input("Enter a number:-"))
-# for i in range(k):
-#     for j in range(k-i):
-#         print("*",end=" ")
-#     print()
-
-# # Enter a number:-5
-# # * * * * *
-# # * * * *
-# # * * *
-# # * *
-# # *
-
-# x=int(input("Enter a number:-"))
-# for i in range(x):
-#     for j in range(x):
-#         if(i<x//2):
-#             if(j<=i):
-#                 print("*",end=" ")
-#         else:
-#             if(j>=i):
-#                 print("*",end=" ")
-#     print()
-            
-# # Enter a number:-5
-# # *
-# # * *
-# # * * *
-# # * *
-# # *   
-
-y=int(input("Enter a number:-"))  
-jloop=y//2
-for i in range(y-1,-1,-1):
-    # print(i)
-    for j in range(jloop+1):
-        if i>=y//2:
-            if j<i//2 and i!=y//2:
-                print("_",end=" ")
+# ----------------Square Hallow Pattern----------------
+n=int(input("Enter a Number:-"))
+for i in range(n):
+    for j in range(n):
+        if(i==0 or i==n-1):
+            print("*",end=" ")
+        else:
+            if j==0 or j==n-1:
+                print("*",end=" ")
             else:
+                print(" ",end=" ")
+    print()
+
+# Enter a Number:-5
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * * 
+
+# ----------Rectangle Half Pyramid   ----------------        
+m=int(input("Enter a number:-"))
+for i in range(1,m+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+    
+# Enter a number:-5
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# ----------------Reversed Right Half Pyramid  ----------------
+k=int(input("Enter a number:-"))
+for i in range(k):
+    for j in range(k-i):
+        print("*",end=" ")
+    print()
+
+# Enter a number:-5
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# ----------------Left Half Pyramid  ----------------
+
+n=int(input("Enter a Number:-"))
+for i in range(n-1,-1,-1):
+    for j in range(n):
+        if(j>=i):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+    
+# Enter a Number:-5
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+# ----------------Reversed Right Half Pyramid  ----------------
+
+n=int(input("Enter a Number:-"))
+for i in range(n):
+    for j in range(n):
+        if(j>=i):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+# Enter a Number:-5
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+        
+# ---------------- Right Pascal's Triangle----------------
+x=int(input("Enter a number:-"))
+for i in range(x):
+    for j in range(x):
+        if(i<x//2):
+            if(j<=i):
                 print("*",end=" ")
         else:
-            if j>i//2 :
+            if(j>=i):
+                print("*",end=" ")
+    print()
+            
+# Enter a number:-5
+# *
+# * *
+# * * *
+# * *
+# *   
+
+# ---------------- Left Pascal's Triangle ----------------
+
+y=int(input("Enter a number:-"))  
+for i in range(y):
+    for j in range((y-1)//2,-1,-1):
+        if(i<=y//2):
+            if(j<=i):
                 print("*",end=" ")
             else:
-                print("_",end=" ")
-                
+                print(" ",end=" ")  
+        else:
+            if j<=(y-1)-i:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")            
             
             
     print()
-                
-                
+           
+# Enter a number:-6
+#     *
+#   * *
+# * * *
+# * * *
+#   * *   
+# Enter a number:-6
+#     *
+#   * *
+# * * *
+# * * *
+#   * *
+#     *  
+
+
+# ----------------Rhombus Pattern ----------------
+  
+m=int(input("Enter a number:-"))   
+for i in range(m):
+    for j in range(m+i):
+        if j<i:
+            print(" ",end=" ")
+        else:
+            print("*",end=" ")
+    print()
+    
+# Enter a number:-5
+# * * * * *
+#   * * * * *
+#     * * * * *
+#       * * * * *
+#         * * * * *
+
+
+        
