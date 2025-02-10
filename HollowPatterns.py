507f9e05ca113fbcefa5b73354e4ea507fde4d56
unknown <rapoluindhu2929@gmail.com>
2025-02-10 22:03:13 +0530

    Patterns

--- HollowPatterns.py
+++ HollowPatterns.py
@@ -1,49 +1,94 @@
-# # ----------------Square Hallow Pattern----------------
-# n=int(input("Enter a Number:-"))
-# for i in range(n):
-#     for j in range(n):
-#         if(i==0 or i==n-1):
-#             print("*",end=" ")
-#         else:
-#             if j==0 or j==n-1:
-#                 print("*",end=" ")
-#             else:
-#                 print(" ",end=" ")
-#     print()
+# ----------------Square Hallow Pattern----------------
+n=int(input("Enter a Number:-"))
+for i in range(n):
+    for j in range(n):
+        if(i==0 or i==n-1):
+            print("*",end=" ")
+        else:
+            if j==0 or j==n-1:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
 
-# # Enter a Number:-5
-# # * * * * *
-# # *       *
-# # *       *
-# # *       *
-# # * * * * * 
-
-
-# # ---------------- Hallow Hourglass Pattern----------------
-
-# n=int(input("Enter odd Number:-"))
-# for i in range(n):
-#     for j in range(n):
-#         if(i==0 or i==n-1):
-#             print("*",end=" ")
-#         else:
-#             if(i==j or j==((n-1)-i)):
-#                 print("*",end=" ")
-#             else:
-#                 print(" ",end=" ")
-#     print()
-
-# # Enter odd Number:-7
-# # * * * * * * *
-# #   *       *   
-# #     *   *
-# #       *
-# #     *   *
-# #   *       *
-# # * * * * * * *
+# Enter a Number:-5
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * * 
 
 
-# # ---------------- Hallow Hourglass Pattern----------------
+# ---------------- Hallow Hourglass Pattern----------------
+
+n=int(input("Enter odd Number:-"))
+for i in range(n):
+    for j in range(n):
+        if(i==0 or i==n-1):
+            print("*",end=" ")
+        else:
+            if(i==j or j==((n-1)-i)):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
+
+# Enter odd Number:-7
+# * * * * * * *
+#   *       *   
+#     *   *
+#       *
+#     *   *
+#   *       *
+# * * * * * * *
 
 
+# ---------------- Hallow Triangle Pattern----------------
+
+n=int(input("Enter a Number:- "))
+for i in range(n):
+    for j in range((n*2)-1):
+        if(i==n-1):
+            if(j%2==0):
+                print("*",end=" ")
+            else:
         
+                print(" ",end=" ")
+        else:
+            if(j==(n-1)-i or j==(n-1)+i):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
+    
+# Enter a Number:- 5
+#         *
+#       *   *
+#     *       *
+#   *           *
+# *   *   *   *   * 
+
+# ---------------- Hallow Reversed Triangle Pattern----------------
+
+n=int(input("Enter a Number:- "))
+for i in range(n-1,-1,-1):
+    for j in range((n*2)-1):
+        if(i==n-1):
+            if(j%2==0):
+                print("*",end=" ")
+            else:
+        
+                print(" ",end=" ")
+        else:
+            if(j==(n-1)-i or j==(n-1)+i):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
+    
+# Enter a Number:- 5
+# *   *   *   *   * 
+#   *           *   
+#     *       *     
+#       *   *       
+#         *  
